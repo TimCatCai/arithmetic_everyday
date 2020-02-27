d7a316ac25bbe56ec8b48ff09df7568468b8d397
timcatcai <--replace>
2020-02-27 15:10:00 +0800

    commit leetcode 155. Min Stack 面试题30.

--- src/timcat/leetcode/xiaoxiang/stack/StackArithmetic.java
+++ src/timcat/leetcode/xiaoxiang/stack/StackArithmetic.java
@@ -236,4 +236,72 @@ public class StackArithmetic {
      */
 
 
+    /**
+     * 面试题30. && 155. 最小栈
+     * 设计一个支持 push，pop，top 操作，并能在常数时间内检索到最小元素的栈。
+     *
+     * push(x) -- 将元素 x 推入栈中。
+     * pop() -- 删除栈顶的元素。
+     * top() -- 获取栈顶元素。
+     * getMin() -- 检索栈中的最小元素。
+     * 示例:
+     *
+     * MinStack minStack = new MinStack();
+     * minStack.push(-2);
+     * minStack.push(0);
+     * minStack.push(-3);
+     * minStack.getMin();   --> 返回 -3.
+     * minStack.pop();
+     * minStack.top();      --> 返回 0.
+     * minStack.getMin();   --> 返回 -2.
+     */
+    class MinStack {
+        private Stack<Integer> dataStack;
+        private Stack<Integer> minStack;
+        /** initialize your data structure here. */
+        public MinStack() {
+            dataStack = new Stack<>();
+            minStack = new Stack<>();
+        }
+
+        public void push(int x) {
+             dataStack.push(x);
+             if(minStack.empty()){
+                 minStack.push(x);
+             }else{
+                 int topInMin = minStack.peek();
+                 if(x <= topInMin){
+                     minStack.push(x);
+                 }else{
+                     minStack.push(topInMin);
+                 }
+             }
+        }
+
+        public void pop() {
+            if(!dataStack.empty()){
+                dataStack.pop();
+            }
+            if(!minStack.empty()){
+                minStack.pop();
+            }
+        }
+
+        public int top() {
+            return dataStack.peek();
+        }
+
+        public int min() {
+            return minStack.peek();
+        }
+    }
+
+    /**
+    * Your MinStack object will be instantiated and called as such:
+    * MinStack obj = new MinStack();
+    * obj.push(x);
+    * obj.pop();
+    * int param_3 = obj.top();
+    * int param_4 = obj.min();
+    */
 }
