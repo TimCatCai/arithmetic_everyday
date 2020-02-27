9a0233e4784482d98394316e2f915ba8202fa30c
timcatcai <--replace>
2020-02-27 12:00:28 +0800

    commit leetcode 225. Implement Stack using Queues && 232. Implement Queue using Stacks

--- src/timcat/leetcode/xiaoxiang/stack/StackArithmetic.java
+++ src/timcat/leetcode/xiaoxiang/stack/StackArithmetic.java
@@ -1,51 +1,56 @@
 package timcat.leetcode.xiaoxiang.stack;
 
+import org.omg.PortableInterceptor.INACTIVE;
+
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 
 public class StackArithmetic {
     /**
      * 20. 有效的括号
      * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
-     *
+     * <p>
      * 有效字符串需满足：
-     *
+     * <p>
      * 左括号必须用相同类型的右括号闭合。
      * 左括号必须以正确的顺序闭合。
      * 注意空字符串可被认为是有效字符串。
-     *
+     * <p>
      * 示例 1:
-     *
+     * <p>
      * 输入: "()"
      * 输出: true
      * 示例 2:
-     *
+     * <p>
      * 输入: "()[]{}"
      * 输出: true
      * 示例 3:
-     *
+     * <p>
      * 输入: "(]"
      * 输出: false
      * 示例 4:
-     *
+     * <p>
      * 输入: "([)]"
      * 输出: false
      * 示例 5:
-     *
+     * <p>
      * 输入: "{[]}"
      * 输出: true
+     *
      * @param s
      * @return
      */
     public boolean isValid(String s) {
-        if(s == null){
+        if (s == null) {
             return false;
         }
         Stack<Character> stack = new Stack<>();
         boolean isMatching = true;
-        for(int i = 0; i < s.length() && isMatching;i++){
+        for (int i = 0; i < s.length() && isMatching; i++) {
             char ch = s.charAt(i);
-            switch (ch){
-                case  '}':
+            switch (ch) {
+                case '}':
                     isMatching = mactchingPair(ch, stack);
                     break;
                 case ')':
@@ -57,23 +62,23 @@ public class StackArithmetic {
                 default:
                     stack.push(ch);
             }
-            if(!isMatching){
+            if (!isMatching) {
                 break;
             }
         }
-        if(!stack.empty()){
+        if (!stack.empty()) {
             isMatching = false;
         }
         return isMatching;
     }
 
-    private boolean mactchingPair(char ch, Stack<Character> stack){
-        if(stack.empty()){
+    private boolean mactchingPair(char ch, Stack<Character> stack) {
+        if (stack.empty()) {
             return false;
         }
         char peek = stack.pop();
-        switch (ch){
-            case  '}':
+        switch (ch) {
+            case '}':
                 return peek == '{';
             case ')':
                 return peek == '(';
@@ -83,4 +88,152 @@ public class StackArithmetic {
                 return false;
         }
     }
+    /**
+     * 225 使用队列实现栈的下列操作：
+     *
+     * push(x) -- 元素 x 入栈
+     * pop() -- 移除栈顶元素
+     * top() -- 获取栈顶元素
+     * empty() -- 返回栈是否为空
+     * 注意:
+     *
+     * 你只能使用队列的基本操作-- 也就是 push to back, peek/pop from front, size, 和 is empty 这些操作是合法的。
+     * 你所使用的语言也许不支持队列。 你可以使用 list 或者 deque（双端队列）来模拟一个队列 , 只要是标准的队列操作即可。
+     * 你可以假设所有操作都是有效的（例如, 对一个空的栈不会调用 pop 或者 top 操作）。
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/implement-stack-using-queues
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
+    /**
+     * Your MyStack object will be instantiated and called as such:
+     * MyStack obj = new MyStack();
+     * obj.push(x);
+     * int param_2 = obj.pop();
+     * int param_3 = obj.top();
+     * boolean param_4 = obj.empty();
+     */
+    class MyStack {
+        private Queue<Integer> queue;
+
+        /**
+         * Initialize your data structure here.
+         */
+        public MyStack() {
+            queue = new LinkedList<>();
+        }
+
+        /**
+         * Push element x onto stack.
+         */
+        public void push(int x) {
+            int size = queue.size();
+            queue.add(x);
+            for (int i = 0; i < size; i++) {
+                queue.add(queue.remove());
+            }
+        }
+
+        /**
+         * Removes the element on top of the stack and returns that element.
+         */
+        public int pop() {
+            return queue.remove();
+        }
+
+        /**
+         * Get the top element.
+         */
+        public int top() {
+            return queue.peek();
+        }
+
+        /**
+         * Returns whether the stack is empty.
+         */
+        public boolean empty() {
+            return queue.isEmpty();
+        }
+    }
+
+    /**
+     * 232. 用栈实现队列
+     * 使用栈实现队列的下列操作：
+     * <p>
+     * push(x) -- 将一个元素放入队列的尾部。
+     * pop() -- 从队列首部移除元素。
+     * peek() -- 返回队列首部的元素。
+     * empty() -- 返回队列是否为空。
+     * 示例:
+     * <p>
+     * MyQueue queue = new MyQueue();
+     * <p>
+     * queue.push(1);
+     * queue.push(2);
+     * queue.peek();  // 返回 1
+     * queue.pop();   // 返回 1
+     * queue.empty(); // 返回 false
+     * 说明:
+     * <p>
+     * 你只能使用标准的栈操作 -- 也就是只有 push to top, peek/pop from top, size, 和 is empty 操作是合法的。
+     * 你所使用的语言也许不支持栈。你可以使用 list 或者 deque（双端队列）来模拟一个栈，只要是标准的栈操作即可。
+     * 假设所有操作都是有效的 （例如，一个空的队列不会调用 pop 或者 peek 操作）。
+     */
+
+    class MyQueue {
+
+        private Stack<Integer> stack;
+        /**
+         * Initialize your data structure here.
+         */
+        public MyQueue() {
+            stack = new Stack<>();
+        }
+
+        /**
+         * Push element x to the back of queue.
+         */
+        public void push(int x) {
+            Stack<Integer> tempStack = new Stack<>();
+            while(!stack.empty()){
+                tempStack.push(stack.pop());
+            }
+            stack.push(x);
+            while(!tempStack.empty()){
+                stack.push(tempStack.pop());
+            }
+        }
+
+        /**
+         * Removes the element from in front of queue and returns that element.
+         */
+        public int pop() {
+            return stack.pop();
+        }
+
+        /**
+         * Get the front element.
+         */
+        public int peek() {
+            return stack.peek();
+        }
+
+        /**
+         * Returns whether the queue is empty.
+         */
+        public boolean empty() {
+            return stack.empty();
+        }
+    }
+
+    /**
+     * Your MyQueue object will be instantiated and called as such:
+     * MyQueue obj = new MyQueue();
+     * obj.push(x);
+     * int param_2 = obj.pop();
+     * int param_3 = obj.peek();
+     * boolean param_4 = obj.empty();
+     */
+
+
 }
